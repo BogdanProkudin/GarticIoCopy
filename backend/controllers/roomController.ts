eac658d94e43803554b388aceb16f47cdba4b6cd
Богдан Прокудин <prokudinbogdan50@gmail.com>
2025-02-11 02:47:53 +0200

    last updated22212121

--- backend/controllers/roomController.ts
+++ backend/controllers/roomController.ts
@@ -120,7 +120,7 @@ export const handleNextUserCall = async (
       const remainingUsers = await updatedRoom?.usersInfo.filter(
         (user: any) => !user.isUserLeave
       );
-      console.log(remainingUsers?.length, "LENGTH REMAIN Users", updatedRoom);
+      console.log(remainingUsers?.length, "LENGTH REMAIN Users1", updatedRoom);
 
       await io
         .to(roomId.length !== 6 ? roomFirstId : roomId)
