2089f1bff005345ac32416dd04b784323dba3148
Богдан Прокудин <prokudinbogdan50@gmail.com>
2025-02-14 16:44:11 +0200

    join logic fixed2w2z

--- DrawIo/src/components/LoadingGame/Loading.tsx
+++ DrawIo/src/components/LoadingGame/Loading.tsx
@@ -1,15 +1,15 @@
 import styles from "../setUpPage/styles.module.scss";
-import Lottie from "react-lottie";
-import LottieSettings from "../../tools/Animation - 1725791635271.json";
+// import Lottie from "react-lottie";
+// import LottieSettings from "../../tools/Animation - 1725791635271.json";
 
-const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  animationData: LottieSettings,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
+// const defaultOptions = {
+//   loop: true,
+//   autoplay: true,
+//   animationData: LottieSettings,
+//   rendererSettings: {
+//     preserveAspectRatio: "xMidYMid slice",
+//   },
+// };
 const Loading = () => {
   return <div className={styles.set_up_loading}>loading</div>;
 };
