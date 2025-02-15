335454266a32944b2bc1d116c95b0e4733c32e84
Богдан Прокудин <prokudinbogdan50@gmail.com>
2025-02-15 03:38:56 +0200

    backend fixed2e

--- DrawIo/src/hooks/useChooseWord.ts
+++ DrawIo/src/hooks/useChooseWord.ts
@@ -5,14 +5,15 @@ export const useChooseWord = (roomId: string) => {
   const handleChooseDrawWord = async (choosedWord: string) => {
     const url = "https://bottg-63go.onrender.com/wordChoosed";
     try {
+      socket.emit("wordChoosed", { choosedWord, roomId });
       const response = await axios.post(url, {
         headers: {
           "Content-Type": "application/json",
         },
         roomId,
       });
+
       if (response.data) {
-        socket.emit("wordChoosed", { choosedWord, roomId });
       }
     } catch (error) {
       console.error("Error:", error);
